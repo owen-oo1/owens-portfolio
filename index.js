c8c9041ea276270a0848a2228360f5bb2bf4fdf3
owen.o.o <owenoothemj8@gmail.com>
2026-01-30 15:08:38 +0300

    updated portfolio content

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
 
 const app = express();
+const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const PORT = 3000;
@@ -51,10 +52,19 @@ app.set('view engine', 'ejs');
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.render("pages/home");
+})
+
+app.get('/about', (req, res) => {
+    res.render("pages/about");
+})
+
+app.get('/contact', (req, res) => {
+    res.render("pages/contact");
 })
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running at port ${PORT}`);
 });
+
